d5ef154927db0658ac8f5fd6331ff75d952aa5f6
Duncan Brown <dabrown@syr.edu>
2021-02-15 19:48:34 -0500

    fixed for correct segment pinout

--- pi-temp-display/test.py
+++ pi-temp-display/test.py
@@ -4,41 +4,33 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
  
-# GPIO ports for the 7seg pins
-segments =  (16,20,21,5,6,13,19,26)
-# 7seg_segment_pins (11,7,4,2,1,10,5,3) +  100R inline
- 
+# GPIO ports for the 7seg pins + dp
+# (b,a,e,d,f,g,dp,c)
+segments =  (5,6,13,19,26,16,20,21)
+
 for segment in segments:
     GPIO.setup(segment, GPIO.OUT)
     GPIO.output(segment, 0)
- 
-# GPIO ports for the digit 0-3 pins 
-#digits = (22,27,17,24)
-# 7seg_digit_pins (12,9,8,6) digits 0-3 respectively
- 
-#for digit in digits:
-#    GPIO.setup(digit, GPIO.OUT)
-#    GPIO.output(digit, 1)
- 
-num = {' ':(0,0,0,0,0,0,0),
-    '0':(1,1,1,1,1,1,0),
-    '1':(0,1,1,0,0,0,0),
-    '2':(1,1,0,1,1,0,1),
-    '3':(1,1,1,1,0,0,1),
-    '4':(0,1,1,0,0,1,1),
-    '5':(1,0,1,1,0,1,1),
-    '6':(1,0,1,1,1,1,1),
-    '7':(1,1,1,0,0,0,0),
-    '8':(1,1,1,1,1,1,1),
-    '9':(1,1,1,1,0,1,1)}
+
+num = {' ':(0,0,0,0,0,0,0,0),
+    '0':(1,1,1,1,1,0,1,1),
+    '1':(1,0,0,0,0,0,1,1),
+    '2':(1,1,1,1,0,1,1,0),
+    '3':(1,1,0,1,0,1,1,1),
+    '4':(1,0,0,0,1,1,1,1),
+    '5':(0,1,0,1,1,1,1,1),
+    '6':(0,1,1,1,1,1,1,1),
+    '7':(1,1,0,0,0,0,1,1),
+    '8':(1,1,1,1,1,1,1,1),
+    '9':(1,1,0,0,1,1,1,1)}
+
  
 try:
     while True:
-        n = 8
-        s = str(n)
-        for digit in range(1):
-            for loop in range(0,7):
-                GPIO.output(segments[loop], num[s[digit]][loop])
+        for n in range(0,9):
+            for loop in range(0,8):
+                GPIO.output(segments[loop], num[str(n)][loop])
             time.sleep(0.001)
+        time.sleep(1)
 finally:
     GPIO.cleanup()
