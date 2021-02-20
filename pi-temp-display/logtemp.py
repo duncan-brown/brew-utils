fd261be1dd0f45054441cb961bde38daae2778f2
Duncan Brown <dabrown@syr.edu>
2021-02-20 11:03:40 -0500

    simple program to print temperature and time

---
+++ pi-temp-display/logtemp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+import time
+
+MASH_PROBE = '/sys/bus/w1/devices/28-012052b65be5/w1_slave'
+
+def get_probe_temp(probe):
+   fileobj = open(probe,'r')
+   lines = fileobj.readlines()
+   fileobj.close()
+   status = lines[0][-4:-1]
+   equals_pos = lines[1].find('t=')
+   tempstr = lines[1][equals_pos+2:]
+   tempvalue_c = float(tempstr)/1000.0
+   tempvalue_f = tempvalue_c * 9.0 / 5.0 + 32.0
+   return tempvalue_f
+
+start_time = time.time()
+
+while True:
+   tempvalue_f = get_probe_temp(MASH_PROBE)
+   tempvalue = round(tempvalue_f,1)
+   print time.time() - start_time, tempvalue
+   time.sleep(30)
