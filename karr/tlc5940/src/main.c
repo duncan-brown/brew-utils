7cd04c58371a0e4d88934c697f83f128761754e9
Duncan Brown <dabrown@syr.edu>
2021-08-29 15:40:37 -0400

    copied from st7789

---
+++ karr/tlc5940/src/main.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_vfs.h"
+#include "esp_spiffs.h"
+
+#include "st7789.h"
+#include "fontx.h"
+#include "bmpfile.h"
+#include "decode_jpeg.h"
+#include "decode_png.h"
+#include "pngle.h"
+
+#define	INTERVAL		400
+#define WAIT	vTaskDelay(INTERVAL)
+
+static const char *TAG = "ST7789";
+
+static void SPIFFS_Directory(char * path) {
+	DIR* dir = opendir(path);
+	assert(dir != NULL);
+	while (true) {
+		struct dirent*pe = readdir(dir);
+		if (!pe) break;
+		ESP_LOGI(__FUNCTION__,"d_name=%s d_ino=%d d_type=%x", pe->d_name,pe->d_ino, pe->d_type);
+	}
+	closedir(dir);
+}
+
+TickType_t ArrowTest(TFT_t * dev, FontxFile *fx, int width, int height) {
+	TickType_t startTick, endTick, diffTick;
+	startTick = xTaskGetTickCount();
+
+	// get font width & height
+	uint8_t buffer[FontxGlyphBufSize];
+	uint8_t fontWidth;
+	uint8_t fontHeight;
+	GetFontx(fx, 0, buffer, &fontWidth, &fontHeight);
+	//ESP_LOGI(__FUNCTION__,"fontWidth=%d fontHeight=%d",fontWidth,fontHeight);
+	
+	uint16_t xpos;
+	uint16_t ypos;
+	int	stlen;
+	uint8_t ascii[24];
+	uint16_t color;
+
+	lcdFillScreen(dev, BLACK);
+
+	strcpy((char *)ascii, "ST7789");
+	if (width < height) {
+		xpos = ((width - fontHeight) / 2) - 1;
+		ypos = (height - (strlen((char *)ascii) * fontWidth)) / 2;
+		lcdSetFontDirection(dev, DIRECTION90);
+	} else {
+		ypos = ((height - fontHeight) / 2) - 1;
+		xpos = (width - (strlen((char *)ascii) * fontWidth)) / 2;
+		lcdSetFontDirection(dev, DIRECTION0);
+	}
+	color = WHITE;
+	lcdDrawString(dev, fx, xpos, ypos, ascii, color);
+
+	lcdSetFontDirection(dev, 0);
+	color = RED;
+	lcdDrawFillArrow(dev, 10, 10, 0, 0, 5, color);
+	strcpy((char *)ascii, "0,0");
+	lcdDrawString(dev, fx, 0, 30, ascii, color);
+
+	color = GREEN;
+	lcdDrawFillArrow(dev, width-11, 10, width-1, 0, 5, color);
+	//strcpy((char *)ascii, "79,0");
+	sprintf((char *)ascii, "%d,0",width-1);
+	stlen = strlen((char *)ascii);
+	xpos = (width-1) - (fontWidth*stlen);
+	lcdDrawString(dev, fx, xpos, 30, ascii, color);
+
+	color = GRAY;
+	lcdDrawFillArrow(dev, 10, height-11, 0, height-1, 5, color);
+	//strcpy((char *)ascii, "0,159");
+	sprintf((char *)ascii, "0,%d",height-1);
+	ypos = (height-11) - (fontHeight) + 5;
+	lcdDrawString(dev, fx, 0, ypos, ascii, color);
+
+	color = CYAN;
+	lcdDrawFillArrow(dev, width-11, height-11, width-1, height-1, 5, color);
+	//strcpy((char *)ascii, "79,159");
+	sprintf((char *)ascii, "%d,%d",width-1, height-1);
+	stlen = strlen((char *)ascii);
+	xpos = (width-1) - (fontWidth*stlen);
+	lcdDrawString(dev, fx, xpos, ypos, ascii, color);
+
+	endTick = xTaskGetTickCount();
+	diffTick = endTick - startTick;
+	ESP_LOGI(__FUNCTION__, "elapsed time[ms]:%d",diffTick*portTICK_RATE_MS);
+	return diffTick;
+}
+
+void ST7789(void *pvParameters)
+{
+	// set font file
+	FontxFile fx16G[2];
+	FontxFile fx24G[2];
+	FontxFile fx32G[2];
+	FontxFile fx32L[2];        
+	InitFontx(fx16G,"/spiffs/ILGH16XB.FNT",""); // 8x16Dot Gothic
+	InitFontx(fx24G,"/spiffs/ILGH24XB.FNT",""); // 12x24Dot Gothic
+	InitFontx(fx32G,"/spiffs/ILGH32XB.FNT",""); // 16x32Dot Gothic
+	InitFontx(fx32L,"/spiffs/LATIN32B.FNT",""); // 16x32Dot Latin
+
+	TFT_t dev;
+	spi_master_init(&dev, CONFIG_MOSI_GPIO, CONFIG_SCLK_GPIO, CONFIG_CS_GPIO, CONFIG_DC_GPIO, CONFIG_RESET_GPIO, CONFIG_BL_GPIO);
+	lcdInit(&dev, CONFIG_WIDTH, CONFIG_HEIGHT, CONFIG_OFFSETX, CONFIG_OFFSETY);
+
+	while(1) {
+
+		ArrowTest(&dev, fx16G, CONFIG_WIDTH, CONFIG_HEIGHT);
+		WAIT;
+
+		ArrowTest(&dev, fx24G, CONFIG_WIDTH, CONFIG_HEIGHT);
+		WAIT;
+
+		ArrowTest(&dev, fx32G, CONFIG_WIDTH, CONFIG_HEIGHT);
+		WAIT;
+
+		ArrowTest(&dev, fx32L, CONFIG_WIDTH, CONFIG_HEIGHT);
+		WAIT;
+
+	} // end while
+
+	// never reach
+	while (1) {
+		vTaskDelay(2000 / portTICK_PERIOD_MS);
+	}
+}
+
+void app_main(void)
+{
+	ESP_LOGI(TAG, "Initializing SPIFFS");
+
+	esp_vfs_spiffs_conf_t conf = {
+		.base_path = "/spiffs",
+		.partition_label = NULL,
+		.max_files = 10,
+		.format_if_mount_failed =true
+	};
+
+	// Use settings defined above toinitialize and mount SPIFFS filesystem.
+	// Note: esp_vfs_spiffs_register is anall-in-one convenience function.
+	esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+	if (ret != ESP_OK) {
+		if (ret == ESP_FAIL) {
+			ESP_LOGE(TAG, "Failed to mount or format filesystem");
+		} else if (ret == ESP_ERR_NOT_FOUND) {
+			ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+		} else {
+			ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)",esp_err_to_name(ret));
+		}
+		return;
+	}
+
+	size_t total = 0, used = 0;
+	ret = esp_spiffs_info(NULL, &total,&used);
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG,"Failed to get SPIFFS partition information (%s)",esp_err_to_name(ret));
+	} else {
+		ESP_LOGI(TAG,"Partition size: total: %d, used: %d", total, used);
+	}
+
+	SPIFFS_Directory("/spiffs/");
+	xTaskCreate(ST7789, "ST7789", 1024*6, NULL, 2, NULL);
+}
