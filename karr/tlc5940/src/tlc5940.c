e76ed3db5b1003436b20a5a053de3f2fde8310f6
Duncan Brown <dabrown@syr.edu>
2021-09-06 13:56:21 -0400

    initial entry

---
+++ karr/tlc5940/src/tlc5940.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "tlc5940.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_err.h"
+#include "esp_log.h"
+
+void tlc5940_init(void)
+{
+    return;
+}
---
+++ karr/tlc5940/src/tlc5940.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_TLC5940_H_
+#define MAIN_TLC5940_H_
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_err.h"
+#include "esp_log.h"
+
+void tlc5940_init(void);
+
+#endif
